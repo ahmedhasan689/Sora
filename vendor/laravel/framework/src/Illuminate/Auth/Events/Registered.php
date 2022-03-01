afcae3e5811710cbc521c329aba83ed185398e81
AhM19eD <hlhatab@gmail.com>
2022-03-01 23:39:49 +0200

    vendor Folder

---
+++ vendor/laravel/framework/src/Illuminate/Auth/Events/Registered.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminate\Auth\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class Registered
+{
+    use SerializesModels;
+
+    /**
+     * The authenticated user.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public $user;
+
+    public $profile;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @return void
+     */
+    public function __construct($user, $profile)
+    {
+        $this->user = $user;
+        $this->profile = $profile;
+    }
+}
